ab871cc79b989ffa5e86fb35af7e5b5a12ff5a21
Mauro <mauro@mauro>
2016-11-15 12:28:46 -0300

    Proyecto funcional

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,27 @@
+
+
+
+import {AuthMethods, AuthProviders} from "angularfire2";
+/*
+export const firebaseConfig = {
+    apiKey: "AIzaSyA0BcUcu4V8aHT_gM-32BhRcmqji4z-lts",
+    authDomain: "final-project-recording.firebaseapp.com",
+    databaseURL: "https://final-project-recording.firebaseio.com",
+    storageBucket: "final-project-recording.appspot.com",
+    messagingSenderId: "290354329688"
+};
+*/
+export const firebaseConfig = {
+    apiKey: "AIzaSyD8iu9tKVjY-rpsZwVu4hMjmZTN0nfYW1s",
+    authDomain: "testfb-fbcdf.firebaseapp.com",
+    databaseURL: "https://testfb-fbcdf.firebaseio.com",
+    storageBucket: "testfb-fbcdf.appspot.com",
+    messagingSenderId: "875765539002"
+};
+
+
+
+export const authConfig = {
+    provider: AuthProviders.Password,
+    method: AuthMethods.Password
+};
